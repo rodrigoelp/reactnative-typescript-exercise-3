f716d9e025806a8c10f848b76c50caae7be450cd
Rodrigo Landaeta <rlandaeta@cochlear.com>
2017-12-02 09:10:16 +1100

    Implemented exercise 3. Navigation between screen passing parameters.
    This sample has a problem and I am contiously navigating to a new screen in the stack.
    
    Follow this exercise if you want to get the basic idea of navigation but the exercise 4 is done in a better way.
    
    Implemented on this exercise:
    
    1. Guid implementation.
    2. Resources and controls

---
+++ src/index.tsx
@@ -0,0 +1,5 @@
+import * as React from "react";
+import { AppRegistry } from "react-native";
+import { AppNavigator } from "./components/AppNavigator";
+
+AppRegistry.registerComponent("ToDo", () => AppNavigator);
